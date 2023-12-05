ef6a7575283aff1b03f87fcf0fe815e17bc150b8
PedroVidalDev <pedrohvidals@gmail.com>
2023-12-05 17:35:29 -0300

    first commit

---
+++ src/main/java/com/pedro/socius/SociusApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.socius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SociusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SociusApplication.class, args);
+	}
+
+}
