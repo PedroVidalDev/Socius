774b57f5001544483bc2756ce0f4d2cdb550801f
PedroVidalDev <pedrohvidals@gmail.com>
2023-12-12 20:02:17 -0300

    [refactor] - criando funcao no controller para receber a requisicao de criacao de agendamento

--- src/main/java/com/pedro/socius/application/controllers/AgendamentoController.java
+++ src/main/java/com/pedro/socius/application/controllers/AgendamentoController.java
@@ -1,7 +1,10 @@
 package com.pedro.socius.application.controllers;
 
+import com.pedro.socius.application.dtos.agendamento.DadosRegistrarAgendamento;
+import com.pedro.socius.domain.AgendamentoService;
 import com.pedro.socius.infrastructure.repositories.AgendamentoRepository;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("agendamento")
+@RequestMapping("agendamentos")
 public class AgendamentoController {
+
+    @Autowired
+    private AgendamentoService service;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity criar(@RequestBody @Valid DadosRegistrarAgendamento dados){
+        service.verificarCriacaoAgendamento(dados);
+
+        return ResponseEntity.ok().build();
+    }
+
 }
