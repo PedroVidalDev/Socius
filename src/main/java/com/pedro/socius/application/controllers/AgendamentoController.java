a58fc156dddf8b6e363126a5274f2b1024432cfa
PedroVidalDev <pedrohvidals@gmail.com>
2023-12-12 20:10:24 -0300

    [feat] - adicionando funcao de deletar um agendamento

--- src/main/java/com/pedro/socius/application/controllers/AgendamentoController.java
+++ src/main/java/com/pedro/socius/application/controllers/AgendamentoController.java
@@ -7,10 +7,7 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("agendamentos")
@@ -19,12 +16,23 @@ public class AgendamentoController {
     @Autowired
     private AgendamentoService service;
 
+    @Autowired
+    private AgendamentoRepository repository;
+
     @PostMapping
     @Transactional
     public ResponseEntity criar(@RequestBody @Valid DadosRegistrarAgendamento dados){
         service.verificarCriacaoAgendamento(dados);
 
         return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity deletar(@PathVariable String id){
+        repository.deleteById(Long.parseLong(id));
+
+        return ResponseEntity.noContent().build();
     }
 
 }
