3b4677fe039593cc30583f0d2bcfc8ebfff83a11
PedroVidalDev <pedrohvidals@gmail.com>
2023-12-12 04:37:58 -0300

    [feat] - adicionando funcao de criar novo local no controller

--- src/main/java/com/pedro/socius/application/controllers/LocalController.java
+++ src/main/java/com/pedro/socius/application/controllers/LocalController.java
@@ -1,9 +1,30 @@
 package com.pedro.socius.application.controllers;
 
+import com.pedro.socius.application.dtos.local.DadosRegistrarLocal;
+import com.pedro.socius.infrastructure.entities.Local;
+import com.pedro.socius.infrastructure.repositories.LocalRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("locais")
 public class LocalController {
+
+    @Autowired
+    private LocalRepository repository;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity registrar(@RequestBody DadosRegistrarLocal dados){
+        Local local = new Local(dados);
+
+        repository.save(local);
+
+        return ResponseEntity.ok().build();
+    }
 }
