f00aac14859852f0a0eaec9adef0d8303d488694
PedroVidalDev <pedrohvidals@gmail.com>
2023-12-12 12:46:45 -0300

    [feat] - criando funcao de atualizar na entidade local e adicionando os campos de dtcreate e dtupdate

--- src/main/java/com/pedro/socius/infrastructure/entities/Local.java
+++ src/main/java/com/pedro/socius/infrastructure/entities/Local.java
@@ -2,6 +2,7 @@ package com.pedro.socius.infrastructure.entities;
 
 import com.google.gson.JsonArray;
 import com.pedro.socius.application.dtos.local.Categoria;
+import com.pedro.socius.application.dtos.local.DadosAtualizarLocal;
 import com.pedro.socius.application.dtos.local.DadosRegistrarLocal;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -9,6 +10,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.mapping.Array;
+
+import java.time.LocalDateTime;
 
 @Entity(name="Local")
 @Table(name="locais")
@@ -29,6 +32,8 @@ public class Local {
 
     private int qntMaxPessoas;
     private int qntHoras;
+    private LocalDateTime dtcreate;
+    private LocalDateTime dtupdate;
 
     // adicionar campo de lista de datas
 
@@ -37,6 +42,30 @@ public class Local {
         this.categoria = dados.categoria();
         this.qntMaxPessoas = dados.qntMaxPessoas();
         this.qntHoras = 0;
+        this.dtcreate = LocalDateTime.now();
+        this.dtupdate = null;
+    }
+
+    public void atualizar(DadosAtualizarLocal dados){
+        String nomeAntigo = this.nome;
+        Categoria categoriaAntiga = this.categoria;
+        int qntMaxPessoasAntiga = this.qntMaxPessoas;
+
+        if(!dados.nome().isBlank()){
+            this.nome = dados.nome();
+        }
+
+        else if(!String.valueOf(dados.categoria()).isBlank()){
+            this.categoria = dados.categoria();
+        }
+
+        else if(!String.valueOf(dados.qntMaxPessoas()).isBlank()){
+            this.qntMaxPessoas = dados.qntMaxPessoas();
+        }
+
+        if(!nomeAntigo.equals(dados.nome()) || !categoriaAntiga.equals(dados.categoria()) || !(qntMaxPessoasAntiga == dados.qntMaxPessoas())){
+            this.dtupdate = LocalDateTime.now();
+        }
     }
 
 }
