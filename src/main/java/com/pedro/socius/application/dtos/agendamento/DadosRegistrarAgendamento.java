4b5a7ba052c0701b1f9aac9f0584c0cd71052b02
PedroVidalDev <pedrohvidals@gmail.com>
2023-12-12 19:13:26 -0300

    [feat] - criando dto para receber os dados de criacao de novos agendamentos

---
+++ src/main/java/com/pedro/socius/application/dtos/agendamento/DadosRegistrarAgendamento.java
@@ -0,0 +1,19 @@
+package com.pedro.socius.application.dtos.agendamento;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record DadosRegistrarAgendamento(
+        @NotNull(message = "ID do local deve ser preenchido.")
+        Long localId,
+        @NotNull(message = "ID do socio deve ser preenchido.")
+        Long socioId,
+        @NotBlank(message = "Data de inicio deve ser preenchida.")
+        LocalDateTime dataInicio,
+        @NotBlank(message = "Data de fim deve ser preenchida.")
+        LocalDateTime dataFim
+) {
+}
