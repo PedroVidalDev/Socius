8ab09ccc8750aac074e8d5fa8d6eae6ebd1814ed
PedroVidalDev <pedrohvidals@gmail.com>
2023-12-12 18:20:17 -0300

    [feat] - adicionando mensagem de erro para o caso de valor invalido na qnt. de pessoas no local

--- src/main/java/com/pedro/socius/application/dtos/local/DadosRegistrarLocal.java
+++ src/main/java/com/pedro/socius/application/dtos/local/DadosRegistrarLocal.java
@@ -1,5 +1,6 @@
 package com.pedro.socius.application.dtos.local;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -11,5 +12,6 @@ public record DadosRegistrarLocal (
         Categoria categoria,
 
         @NotNull(message = "Qnt. de pessoas nao deve ser nula ou vazia.")
+        @Min(value = 0, message = "Valor minimo é zero.")
         int qntMaxPessoas){
 }
