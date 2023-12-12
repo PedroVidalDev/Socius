1aab8b42500b610bbfc4c25a11378c0abe897711
PedroVidalDev <pedrohvidals@gmail.com>
2023-12-12 20:03:16 -0300

    [refactor] - adicionando na service funcao de validar os IDs e datas, alem da funcao de salvar no banco de dados

--- src/main/java/com/pedro/socius/domain/AgendamentoService.java
+++ src/main/java/com/pedro/socius/domain/AgendamentoService.java
@@ -1,7 +1,48 @@
 package com.pedro.socius.domain;
 
+import com.pedro.socius.application.dtos.agendamento.DadosRegistrarAgendamento;
+import com.pedro.socius.infrastructure.entities.Agendamento;
+import com.pedro.socius.infrastructure.repositories.AgendamentoRepository;
+import com.pedro.socius.infrastructure.repositories.LocalRepository;
+import com.pedro.socius.infrastructure.repositories.SocioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @Service
 public class AgendamentoService {
+
+    @Autowired
+    private LocalRepository localRepository;
+
+    @Autowired
+    private SocioRepository socioRepository;
+
+    @Autowired
+    private AgendamentoRepository agendamentoRepository;
+
+    public void verificarCriacaoAgendamento(DadosRegistrarAgendamento dados) {
+        var local = localRepository.getReferenceById(dados.localId());
+        var socio = socioRepository.getReferenceById(dados.socioId());
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+        var dataInicio = LocalDateTime.parse(dados.dataInicio(), formatter);
+        var dataFim = LocalDateTime.parse(dados.dataFim(), formatter);
+
+
+        if(dataInicio.isAfter(LocalDateTime.now())){
+            if(dataFim.isAfter(dataInicio) && dataFim.isBefore(dataInicio.plusDays(1))){
+                Agendamento agendamento = new Agendamento(null, local, socio, dataInicio, dataFim);
+
+                agendamentoRepository.save(agendamento);
+            }
+        }
+
+
+    }
 }
