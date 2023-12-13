475b804f08445ecb7533faad62eba8276ac685f9
PedroVidalDev <pedrohvidals@gmail.com>
2023-12-13 15:05:30 -0300

    [feat] - criando funcao em AgendamentoRepository para auxiliar nos relatorios e regras de negocio do proprio agendamento

--- src/main/java/com/pedro/socius/infrastructure/repositories/AgendamentoRepository.java
+++ src/main/java/com/pedro/socius/infrastructure/repositories/AgendamentoRepository.java
@@ -1,6 +1,7 @@
 package com.pedro.socius.infrastructure.repositories;
 
 import com.pedro.socius.infrastructure.entities.Agendamento;
+import org.hibernate.sql.results.graph.embeddable.internal.AggregateEmbeddableFetchImpl;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -8,6 +9,11 @@ import java.util.List;
 
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
     boolean existsByDataInicioBetween(LocalDateTime dataInicio, LocalDateTime dataFim);
+    boolean existsByDataFimBetween(LocalDateTime dataInicio, LocalDateTime dataFim);
+
 
     List<Agendamento> findAllByLocalId(Long id);
+
+    List<Agendamento> findAllBySocioId(Long id);
+
 }
